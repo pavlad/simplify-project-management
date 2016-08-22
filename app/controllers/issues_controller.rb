67bbccf8fd2fb85a5dd99d39f5c850b0360fad03
Lukas Gutwinski <lukas@workbeat.at>
2016-08-22 17:56:09 +0200

    Finished most of it waiting for projects CRUD

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,52 @@
+class IssuesController < ApplicationController
+  before_action :find_project, only: [:new, :create, :destroy]
+
+  def index
+    @issues = Issue.all
+  end
+
+  def show
+  end
+
+  def new
+    @issue = Issue.new
+  end
+
+  def create
+    @issue = @project.issues.build(issue_params)
+    @issue.save
+    redirect_to project_path(@project)
+  end
+
+  def edit
+  end
+
+  def update
+    @issue.update(issue_params)
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @issue.destroy
+    redirect_to project_path
+  end
+
+  def mark_as_resolved
+    @issue.is_resolved = true
+  end
+
+  def unmark_as_resolved
+    @issue.is_resolved = true
+  end
+
+  private
+
+  def issue_params
+    params.require(:issue).permit(:label, :description, :comment)
+  end
+
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
+
+end
