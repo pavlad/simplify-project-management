4bafb14030d9853275a4f401dbefdeea818bc9aa
Phanarat Pak <phanpak@gmail.com>
2016-08-22 16:03:59 +0200

    rename the model to Assignment

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,8 @@ class Project < ApplicationRecord
   # belongs_to :client
   has_many :issues
   has_many :tasks
-  has_many :consultant_projects
-  has_many :consultants, through: :consultant_projects
+  has_many :assignments
+  has_many :consultants, through: :assignments
   belongs_to :project_manager, class_name: "User", foreign_key: :project_manager_id
   validates :name, presence: true
 end
