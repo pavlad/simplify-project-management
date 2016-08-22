b4d9a25b06d7f7ea070ba8ee6a6c09abf0fd8e95
Vladimir Pavlyukov <vpavlyu@gmail.com>
2016-08-22 18:53:44 +0200

    Added mark and unmark as done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :set_project, only: [:create, :update, :new, :edit, :index, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :mark_as_done, :unmark_as_done]
+  before_action :set_project, only: [:create, :update, :new, :edit, :index, :destroy, :mark_as_done, :unmark_as_done]
 
   def index
     @tasks = Task.all
@@ -11,7 +11,6 @@ class TasksController < ApplicationController
 
   def edit
     @users = User.all
-
   end
 
   def create
@@ -37,6 +36,18 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
 
+    redirect_to project_tasks_path(@project)
+  end
+
+  def mark_as_done
+    @task.is_done = true
+    @task.save
+    redirect_to project_tasks_path(@project)
+  end
+
+  def unmark_as_done
+    @task.is_done = false
+    @task.save
     redirect_to project_tasks_path(@project)
   end
 
