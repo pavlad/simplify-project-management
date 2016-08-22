b2d43aced93162efdae68baab9567c8532ecc42f
Vladimir Pavlyukov <vpavlyu@gmail.com>
2016-08-22 14:33:41 +0200

    Pushing routes

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,19 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :projects do
+    resources :tasks
+    resources :issues
+  end
+  resources :users do
+    get 'projects', on: :member
+    get 'tasks', on: :member
+  end
+  resources :clients
+
 end
