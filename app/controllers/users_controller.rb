06189003015cdd25de0cc4fad1fc1061a3d4c59c
Vladimir Pavlyukov <vpavlyu@gmail.com>
2016-08-23 22:02:15 +0200

    Created Consultants(users) page, create users button doesn't work yet. Updated links in navbar, current_user name and title is now displayed.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+
+    redirect_to users_path
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :job_title, :mobile_phone, :phone, :description, :is_owner )
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,6 @@ Rails.application.routes.draw do
   namespace :user do
     resources :tasks
   end
+
+
 end
