4dd4bd5608556fa3a37b72a0ca9679a411fc06c6
Vladimir Pavlyukov <vpavlyu@gmail.com>
2016-08-25 23:05:38 +0200

    Implemented basic Algolia method to models.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,9 @@
 class Client < ApplicationRecord
   has_many :projects
   validates :name, uniqueness: true, presence: true
+
+  include AlgoliaSearch
+  algoliasearch do
+    attribute :name, :phone, :address, :website, :email
+  end
 end
