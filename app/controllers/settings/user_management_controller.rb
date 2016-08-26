b26e276afe858df4a9a26ca7f4e86a19c4d5dfc9
Lukas Gutwinski <lukas@workbeat.at>
2016-08-26 14:53:21 +0200

    Added link to navbar

--- app/controllers/settings/user_management_controller.rb
+++ app/controllers/settings/user_management_controller.rb
@@ -1,4 +1,4 @@
-class UserManagementController < ApplicationController
+class Settings::UserManagementController < ApplicationController
   def index
     @users = User.where.not(last_sign_in_at: nil)
     @pending_user_invitations = User.where(invitation_accepted_at: nil, last_sign_in_at: nil, is_client: false)
