d8906ca1286400fccbe4e7f7f13228d7723b78b7
Lukas Gutwinski <lukas@workbeat.at>
2016-08-27 11:13:23 +0200

    Changed Devise Invitable controller to accept first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, controllers: { invitations: "users/invitations" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects do
