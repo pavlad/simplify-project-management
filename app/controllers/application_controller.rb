59ce23739b203ecdfee29c7c20e55cf3ad855ca7
Vladimir Pavlyukov <vpavlyu@gmail.com>
2016-10-01 20:48:48 +0200

    Disbled required Devise authentication to allow demo visitors to check out the applicaiton. Changed Application and Passthrough controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   helper_method :xeditable?
   # Pundit: white-list approach.
 
--- app/controllers/passthrough_controller.rb
+++ app/controllers/passthrough_controller.rb
@@ -1,6 +1,6 @@
 class PassthroughController < ApplicationController
   def index
-    if current_user.is_client
+    if current_user.present? && current_user.is_client
       path = client_dashboard_index_path
     else
       path = overview_dashboard_index_path
